9c24c9bb5fd99bc2dd07a0c98e0d64ddcb625739
amir16yp <96111605+amir16yp@users.noreply.github.com>
2023-04-19 17:22:17 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-sudo apt-get update
-sudo apt-get install sox
-sudo apt-get install libsox-fmt-all libsox-fmt-mp3
-pip install flask flask_uploads flask_cors tinytag
-sudo chmod 777 pifmrds
+#sudo apt-get update
+sudo apt-get install libsox-fmt-all libsox-fmt-mp3 sox git libsndfile1-dev
+pip install flask Flask-Reuploaded flask_cors tinytag
+git clone https://github.com/ChristopheJacquet/PiFmRds.git
+cd ./PiFmRds/src
+make
+sudo cp ./pi_fm_rds /usr/local/bin/pifmrds
+sudo chmod +x /usr/local/bin/pifmrds
+cd ../../
 cd static/audio/
-sudo chmod 777 pifmrds
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import signal
 import datetime
 import os
 import time
-
+from waitress import serve
 app = Flask(__name__, static_url_path='/static')
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -38,22 +38,27 @@ def start():
     json = request.get_json()
     file_name = json["file_name"]
     freq = json["freq"]
-    file = TinyTag.get("static/audio/" + file_name, image=True)
-    radio_text = removeNonAscii(file.title)
-    station_name = removeNonAscii(file.artist)
-
-    # m = subprocess.Popen("./pifmrds -audio " + file_name + " -freq " + freq + " -rt " + radio_text)
-    # m.wait()
+    try:
+        file = TinyTag.get("static/audio/" + file_name, image=True)
+        radio_text = removeNonAscii(file.title)
+        station_name = removeNonAscii(file.artist)
+    except:
+        radio_text = 'Raspberry'
+        station_name = 'Pi Radio'
     if pifm_proc and not pifm_proc.poll():
         print("Killing")
-        # os.killpg(os.getpgid(pifm_proc.pid), signal.SIGTERM)
         subprocess.Popen("sudo killall pifmrds", shell=True)
         print("Killed")
         pifm_proc = None
 
-    cmd = "sox -t mp3 {} -t wav - | sudo ./pifmrds -audio - -freq {} -rt '{}' -ps '{}'".format(file_name, freq, radio_text, station_name) 
-    print("Cmd: {}".format(cmd))
-    pifm_proc = subprocess.Popen(cmd, shell=True, cwd="static/audio", preexec_fn=os.setsid)
+    if file_name != 'Microphone':
+        cmd = 'sox -t mp3 static/audio/{} -t wav - | sudo pifmrds -audio - -freq {} -rt "{}"'.format(file_name, freq, radio_text) 
+        print("Cmd: {}".format(cmd))
+        pifm_proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+    else:
+        cmd = 'sudo arecord -fS16_LE -r 44100 -Dplughw:1,0 -c 2 -  | sudo pifmrds -audio - -freq {} -rt "{}"'.format(freq, radio_text) 
+        print("Cmd: {}".format(cmd))
+        pifm_proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
 
     playing_file = file_name
     start_time = time.time()
@@ -74,20 +79,19 @@ def starturl():
     file_name = json["file_name"]
     freq = json["freq"]
     radio_text = json["radio_text"]
-    # radio_text = "web-rpi-fm"
-    # m = subprocess.Popen("./pifmrds -audio " + file_name + " -freq " + freq + " -rt " + radio_text)
-    # m.wait()
     if pifm_proc and not pifm_proc.poll():
         print("Killing")
-        # os.killpg(os.getpgid(pifm_proc.pid), signal.SIGTERM)
         subprocess.Popen("sudo killall pifmrds", shell=True)
         print("Killed")
         pifm_proc = None
-
-    cmd = "sox -t mp3 {} -t wav - | sudo ./pifmrds -audio - -freq {} -rt {}".format(file_name, freq, radio_text) 
-    print("Cmd: {}".format(cmd))
-    pifm_proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
-
+    if file_name != 'Microphone':
+        cmd = 'sox -t mp3 static/audio/{} -t wav - | sudo /static/audio/pifmrds -audio - -freq {} -rt "{}"'.format(file_name, freq, radio_text) 
+        print("Cmd: {}".format(cmd))
+        pifm_proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+    else:
+        cmd = 'sudo arecord -fS16_LE -r 44100 -Dplughw:1,0 -c 2 -  | sudo /static/audio/pifmrds/pifmrds -audio - -freq {} -rt "{}"'.format(freq, radio_text) 
+        print("Cmd: {}".format(cmd))
+        pifm_proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
     playing_file = file_name
     start_time = time.time()
 
@@ -96,7 +100,7 @@ def starturl():
 @app.route('/<path:path>')
 def send_static(path):
     if not os.path.isfile("static/" + path):
-        return app.send_static_file('index.html')    
+        return app.send_static_file('index.html')
     return send_from_directory('static', path)
 
 @app.route('/')
@@ -109,7 +113,6 @@ def stop():
 
     if pifm_proc and not pifm_proc.poll():
         print("Killing")
-        # os.killpg(os.getpgid(pifm_proc.pid), signal.SIGTERM)
         subprocess.Popen("sudo killall pifmrds", shell=True)
         print("Killed")
         pifm_proc = None
@@ -140,22 +143,32 @@ def status():
         }), 200
     
     file = playing_file
-    f = TinyTag.get("static/audio/" + file, image=True)
-    if not f.title:
-        f.title = file
-
     img_path = None
-    
-    img_exists = os.path.isfile("static/img/" + file.split(".")[0] + ".png")
-    if img_exists:
-        img_path = file.split(".")[0] + ".png"
+    title = None
+    length = None
+    author = None
+    try:
+        f = TinyTag.get("static/audio/" + file, image=True)
+        
+        title = f.title
+        length = f.duration
+        author = f.artist
+        if not f.title:
+            f.title = file
 
+        img_path = None
+        
+        img_exists = os.path.isfile("static/img/" + file.split(".")[0] + ".png")
+        if img_exists:
+            img_path = file.split(".")[0] + ".png"
+    except:
+        pass
     return jsonify({
         "running": True,
         "filename": file,
-        "name": f.title,
-        "length": f.duration,
-        "author": f.artist,
+        "name": title,
+        "length": length,
+        "author": author,
         "img": img_path,
         "time_elapsed": time.time() - start_time,
     }), 200
@@ -167,10 +180,6 @@ def sysinfo():
 @app.route('/ls')
 def file_list():
     payload = {}
-    # p = subprocess.Popen("ls", stdout=subprocess.PIPE, shell=True, cwd="static/audio")
-    # p.wait()
-    # files = p.stdout.readlines()
-    # files = [x.strip() for x in files]
     i = 0
     for r, d, f in os.walk("static/audio"):
         for f2 in f:
@@ -195,7 +204,7 @@ def file_list():
             except:
                 pass
     return jsonify(payload)
-    
+
 @app.route('/upload', methods=["POST"])
 def upload():
     if request.method == "POST" and 'audio' in request.files:
@@ -204,7 +213,7 @@ def upload():
         image_data = f.get_image()
         if image_data:
             image_name = filename.split(".")[0]
-            with file("static/img/" + image_name + ".png", 'wb') as x:
+            with open("static/img/" + image_name + ".png", 'wb') as x:
                 x.write(image_data)
         return filename
 
@@ -224,7 +233,4 @@ def delete():
 
 if __name__ == "__main__":
     subprocess.Popen("sudo killall pifmrds", shell=True)
-    app.run(port=9000, host='0.0.0.0')
-
-# p = subprocess.Popen("sox -t mp3 nighoffire.mp3 -t wav - | sudo ./pi_fm_adv --audio - --freq 88", stdout=subprocess.PIPE, shell=True, cwd="static/audio")
-# p.wait()
+    serve(app, host='0.0.0.0', port=9000)
