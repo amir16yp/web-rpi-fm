912b79bb41c8cac3f6ed668091c2661110eaaea9
amir16yp <96111605+amir16yp@users.noreply.github.com>
2023-04-19 17:32:05 +0300

    make it install pip aswell

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install libsox-fmt-all libsox-fmt-mp3 sox git libsndfile1-dev
+sudo apt-get install libsox-fmt-all libsox-fmt-mp3 sox git libsndfile1-dev python3-pip
 pip install flask Flask-Reuploaded flask_cors tinytag
 git clone https://github.com/ChristopheJacquet/PiFmRds.git
 cd ./PiFmRds/src
