b123e650c8f18e81fdc56f9ea0841dd4587b0b23
amir16yp <96111605+amir16yp@users.noreply.github.com>
2023-04-19 17:23:09 +0300

    fixed an oopsie

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#sudo apt-get update
+sudo apt-get update
 sudo apt-get install libsox-fmt-all libsox-fmt-mp3 sox git libsndfile1-dev
 pip install flask Flask-Reuploaded flask_cors tinytag
 git clone https://github.com/ChristopheJacquet/PiFmRds.git
@@ -7,4 +7,3 @@ make
 sudo cp ./pi_fm_rds /usr/local/bin/pifmrds
 sudo chmod +x /usr/local/bin/pifmrds
 cd ../../
-cd static/audio/
